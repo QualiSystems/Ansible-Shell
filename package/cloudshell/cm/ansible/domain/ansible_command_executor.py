d4bda26985adcd8c9473798f2e484c1b02e84620
asafc <asafc64@gmail.com>
2016-12-19 17:55:26 +0200

    Clear comments in ansible_command_executor

--- package/cloudshell/cm/ansible/domain/ansible_command_executor.py
+++ package/cloudshell/cm/ansible/domain/ansible_command_executor.py
@@ -5,7 +5,10 @@ from logging import Logger
 from cloudshell.api.cloudshell_api import CloudShellAPISession
 from cloudshell.shell.core.context import ResourceCommandContext
 
+
 class AnsibleCommandExecutor(object):
+    CHUNK_TO_READ = 512
+
     def __init__(self, output_parser, output_writer):
         """
         :type output_parser: AnsiblePlaybookParser
@@ -14,7 +17,7 @@ class AnsibleCommandExecutor(object):
         self.output_parser = output_parser
         self.output_writer = output_writer
 
-    def execute_playbook(self, playbook_file, inventory_file, logger, args = None):
+    def execute_playbook(self, playbook_file, inventory_file, logger, args=None):
         """
         :type playbook_file: str
         :type inventory_file: str
@@ -24,27 +27,23 @@ class AnsibleCommandExecutor(object):
         """
         shellCommand = self._createShellCommand(playbook_file, inventory_file, args)
 
-        logger.info('Running cmd \'%s\' ...'%shellCommand)
+        logger.info('Running cmd \'%s\' ...' % shellCommand)
         start_time = time.time()
         process = subprocess.Popen(shellCommand, shell=True, stdout=subprocess.PIPE)
-        output=''
-        CUNK_TO_READ = 512
+        output = ''
 
         while True:
-            # line = process.stdout.readline()
-            # if not line:
-            #     break;
-            pOut = process.stdout.read(CUNK_TO_READ)
+            txt = process.stdout.read(AnsibleCommandExecutor.CHUNK_TO_READ)
             if process.poll() is not None:
-               break
-            self.output_writer.write(pOut)
-            output += pOut
-            #TODO: write to output window. via api command
-        # output = subprocess.check_output(shellCommand)
-        elapsed = time.time()-start_time
+                break
+            self.output_writer.write(txt)
+            output += txt
+
+        elapsed = time.time() - start_time
         line_count = len(output.split(os.linesep))
         logger.info('Done (after \'%s\' sec, with %s lines of output).' % (elapsed, line_count))
         logger.debug(output)
+
         return self.output_parser.parse(output)
 
     def _createShellCommand(self, playbook_file, inventory_file, args):
