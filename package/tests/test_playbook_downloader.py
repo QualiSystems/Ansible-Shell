afcd2b2351888d3db59d0f68c29286d9f731ac1d
yoavma <yoav.m@quali.com>
2016-12-27 15:52:46 +0200

    fix test

--- package/tests/test_playbook_downloader.py
+++ package/tests/test_playbook_downloader.py
@@ -56,15 +56,11 @@ class TestPlaybookDownloader(TestCase):
         self.assertEqual(e.exception.message,"Playbook file name was not found in zip file")
 
     def _test_playbook_downloader_with_one_yaml(self):
-        self.zip_service.extract_all = lambda zip_file_name: self._set_extract_all_zip(["lie.yaml", "site.yaml"])
         auth = HttpAuth("user", "pass")
-        self.reqeust.url = "blabla/lie.zip"
-        dic = dict([('content-disposition', 'lie.zip')])
+        self.reqeust.url = "blabla/lie.yaml"
+        dic = dict([('content-disposition', 'lie.yaml')])
         self.reqeust.headers = dic
         self.reqeust.iter_content.return_value = ''
         self.http_request_serivce.get_request.return_value = self.reqeust
         file_name = self.playbook_downloader.get("", auth, self.logger)
-        self.assertEquals(file_name, "site.yaml")
-
-
-
+        self.assertEquals(file_name, "lie.yaml")
