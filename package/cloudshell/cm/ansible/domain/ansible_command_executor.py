140679fc355d29d796e1daad3c0402ca93dc1b82
asafc64 <asafc64@gmail.com>
2016-12-29 07:33:22 +0200

    Removed cloudshell_session_provider

--- package/cloudshell/cm/ansible/domain/ansible_command_executor.py
+++ package/cloudshell/cm/ansible/domain/ansible_command_executor.py
@@ -10,6 +10,8 @@ from cloudshell.cm.ansible.domain.output.unixToHtmlConverter import UnixToHtmlCo
 from cloudshell.cm.ansible.domain.file_system_service import FileSystemService
 from cloudshell.shell.core.context import ResourceCommandContext
 
+from cloudshell.cm.ansible.domain.stdout_accumulator import StdoutAccumulator
+
 
 class AnsibleCommandExecutor(object):
     def __init__(self, output_parser):
@@ -67,44 +69,6 @@ class AnsibleCommandExecutor(object):
         if args:
             command += " " + args
         return command
-
-
-class StdoutAccumulator(object):
-    def __init__(self, stdout):
-        self.queue = Queue()
-        self.stdout = stdout
-        self.thread = Thread(target=self._push_to_queue)
-        self.thread.daemon = True
-        self.lock = RLock()
-
-    def __enter__(self):
-        self.thread.start()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        with self.lock:
-            self.stdout.close()
-        self.thread.join()
-
-    def _push_to_queue(self):
-        is_closed = False
-        while not is_closed:
-            with self.lock:
-                is_closed = self.stdout.closed
-                if not is_closed:
-                    line = self.stdout.readline()
-                    if line:
-                        self.queue.put(line)
-
-    def read_all_txt(self):
-        try:
-            lines = []
-            while True:
-                lines.append(self.queue.get_nowait())
-        except Empty:
-            pass
-        finally:
-            return os.linesep.join(lines)
 
 
 class OutputWriter(object):
