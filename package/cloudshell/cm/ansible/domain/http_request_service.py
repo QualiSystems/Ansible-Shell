ce244ce3d92b817cac15ca8ebb1be576b7895fba
katzy687 <katzy687@gmail.com>
2020-12-24 19:24:36 +0200

    removed authentication on requests call

--- package/cloudshell/cm/ansible/domain/http_request_service.py
+++ package/cloudshell/cm/ansible/domain/http_request_service.py
@@ -3,4 +3,5 @@ import requests
 
 class HttpRequestService(object):
     def get_response(self, url, auth):
-        return requests.get(url, auth=(auth.username, auth.password) if auth else None, stream=True)
+        # return requests.get(url, auth=(auth.username, auth.password) if auth else None, stream=True)
+        return requests.get(url, auth=(auth.username, auth.password) if auth else None, stream=True, verify=False)
