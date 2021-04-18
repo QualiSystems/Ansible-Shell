4b5307cf2ebc99cc4067179fab48e29570108405
QUALISYSTEMS\nahum-t <ntimerman@gmail.com>
2021-04-18 16:47:43 +0300

    fix tests

--- package/tests/test_unixToHtml_converter.py
+++ package/tests/test_unixToHtml_converter.py
@@ -8,20 +8,20 @@ class TestUnixToHtmlColorConverter(TestCase):
 
     def test_convert_all_colors(self):
         text = 'i am white'
-        expectedText = '<html><body><font color=white>i am white'
+        expectedText = '<html><body>i am white'
         for key,value in self.color_converter.unixToHtml.items():
             representing_text = 'i am '
             text += key.replace("\\","") + representing_text + value
-            expectedText+='</font><font color=' + value + '>' + representing_text + value
-        expectedText+='</font></body></html>'
+            expectedText+='<font color=' + value + '>' + representing_text + value +</font>
+        expectedText+='</body></html>'
         self.assertEqual(self.color_converter.convert(text),expectedText)
 
     def test_convert_no_color(self):
         text='i am text with no colors'
-        expectedText = '<html><body><font color=white>' + text + '</font></body></html>'
+        expectedText = '<html><body>' + text + '</body></html>'
         self.assertEqual(self.color_converter.convert(text), expectedText)
 
     def test_convert_empty_text(self):
         text = ''
-        expectedText = '<html><body><font color=white></font></body></html>'
+        expectedText = '<html><body></body></html>'
         self.assertEqual(self.color_converter.convert(text), expectedText)
